fcf7e25251c2c89f7ad55a1f35538d4bd29084d0
Kutubuddin-Rasel <raselvai257@gmail.com>
2026-02-28 04:16:48 +0600

    feat: add role guard

---
+++ backend/src/modules/auth/guards/roles.guard.ts
@@ -0,0 +1,43 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from 'generated/prisma/enums';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { JwtPayload } from 'src/modules/interfaces/jwt.interface';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest<{ user: JwtPayload }>();
+
+    if (!user) {
+      throw new ForbiddenException(
+        'User context is missing, Ensure JwtAuthGuard is appplied first.',
+      );
+    }
+
+    const hasRole = requiredRoles.includes(user.role);
+
+    if (!hasRole) {
+      throw new ForbiddenException(
+        `Access denied. Requires role one of these roles ${requiredRoles.join(',')} `,
+      );
+    }
+    return true;
+  }
+}
