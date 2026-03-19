ce6b9256a577658c2270c2a70503a40cd116b17c
Kutubuddin-Rasel <raselvai257@gmail.com>
2026-03-20 00:43:47 +0600

    feat: update bootstrap function signature to return a promise and use void for invocation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './common/filters/all-exceptions.filter';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
@@ -15,4 +15,4 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
