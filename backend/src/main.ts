f8e7148225175a60dfe44e12675e1acb3ae7d724
Kutubuddin-Rasel <raselvai257@gmail.com>
2026-03-23 23:29:32 +0600

    feat: add BigIntInterceptor to handle bigint serialization in global interceptors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './common/filters/all-exceptions.filter';
+import { BigIntInterceptor } from './common/interceptors/bigInt.interceptor';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap(): Promise<void> {
     }),
   );
   app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new BigIntInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
