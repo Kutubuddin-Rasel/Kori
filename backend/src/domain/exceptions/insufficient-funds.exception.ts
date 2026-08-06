cb795cb6ff0ec22b8aad5e0dcb0b2c6fcb8f8d89
Kutubuddin-Rasel <raselvai257@gmail.com>
2026-08-07 01:24:59 +0600

    feat: add InsufficientFundsException for handling insufficient funds errors

---
+++ backend/src/domain/exceptions/insufficient-funds.exception.ts
@@ -0,0 +1,9 @@
+export class InsufficientFundsException extends Error {
+  constructor(available: bigint, required: bigint) {
+    super(
+      `Insufficient funds: wallet has ${available}, but ${required} is required`,
+    );
+    this.name = 'InsufficientFundsException';
+    Object.setPrototypeOf(this, InsufficientFundsException.prototype);
+  }
+}
