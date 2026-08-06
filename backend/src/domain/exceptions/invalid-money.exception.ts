ac644c68522ee18310e12019336865c7096cdef2
Kutubuddin-Rasel <raselvai257@gmail.com>
2026-08-07 01:25:13 +0600

    feat: add InvalidMoneyException for handling invalid money errors

---
+++ backend/src/domain/exceptions/invalid-money.exception.ts
@@ -0,0 +1,15 @@
+/**
+ * Domain exception - Zero framework imports
+ * Extend built-in Error
+ * The application layer (Use Cases) catches this and maps it to BadRequestException
+ */
+export class InvalidMoneyExcepton extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'InvalideMoneyException';
+
+    // Required in TypeScript when extending built-in Error.
+    // Without this, instanceof checks fail in some runtimes.
+    Object.setPrototypeOf(this, InvalidMoneyExcepton.prototype);
+  }
+}
