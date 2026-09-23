ecb9c4aa95bda24e8d0ebb8738837e7ccc9588b4
Kutubuddin-Rasel <raselvai257@gmail.com>
2026-09-23 21:55:41 +0600

    fix: correct typo in recipientPhone property name in SendMoneyDto

--- backend/src/modules/transactions/dto/send-money.dto.ts
+++ backend/src/modules/transactions/dto/send-money.dto.ts
@@ -4,5 +4,5 @@ import { BaseTransactionDto } from './base-transaction.dto';
 export class SendMoneyDto extends BaseTransactionDto {
   @IsNotEmpty()
   @IsPhoneNumber('BD')
-  recipetPhone: string = '';
+  recipientPhone: string = '';
 }
